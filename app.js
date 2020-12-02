aca47099dd7664280f46e37df896a809311245eb
Faysal Ibn Mizan <ovitahsan@gmail.com>
2020-12-02 06:47:44 +0100

    add callback function

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Boston', (error, data) => {
-  console.log('Error', error);
-  console.log('Data', data);
-});
+const address = process.argv[2];
 
+if (!address) {
+  console.log('Please provide an address');
+} else {
+  geocode(address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return console.log(error);
+    }
 
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
 
-forecast(44.1545, -75.7088, (error, data) => {
-  console.log('Error', error);
-  console.log('Data', data);
-});
+      console.log(location);
+      console.log(forecastData);
+    });
+  });
+}
